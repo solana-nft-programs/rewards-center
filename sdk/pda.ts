26bf589da02fc93917844c8c50e87638126255e6
jpbogle <jpbogle22@gmail.com>
2022-11-14 12:29:54 -0500

    Cleanup pda and tests

--- sdk/pda.ts
+++ sdk/pda.ts
@@ -2,7 +2,7 @@ import { utils } from "@project-serum/anchor";
 import { findProgramAddressSync } from "@project-serum/anchor/dist/cjs/utils/pubkey";
 import type { PublicKey } from "@solana/web3.js";
 
-import { PROGRAM_ID } from "./generated/stakePool";
+import { stakePool } from "./";
 
 export const findStakeEntryId = (
   stakePoolId: PublicKey,
@@ -14,7 +14,7 @@ export const findStakeEntryId = (
       stakePoolId.toBuffer(),
       mintId.toBuffer(),
     ],
-    PROGRAM_ID
+    stakePool.PROGRAM_ID
   )[0];
 };
 
@@ -24,13 +24,13 @@ export const findStakePoolId = (identifier: string): PublicKey => {
       utils.bytes.utf8.encode("stake-pool"),
       utils.bytes.utf8.encode(identifier),
     ],
-    PROGRAM_ID
+    stakePool.PROGRAM_ID
   )[0];
 };
 
 export const findUserEscrowId = (user: PublicKey): PublicKey => {
   return findProgramAddressSync(
     [utils.bytes.utf8.encode("escrow"), user.toBuffer()],
-    PROGRAM_ID
+    stakePool.PROGRAM_ID
   )[0];
 };
