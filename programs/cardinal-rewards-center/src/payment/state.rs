255ba1bd7fd961b386f25c5e1b6a0dbab8059ee8
Giannis Chatziveroglou <gchatz@mit.edu>
2022-12-21 19:06:36 +0200

    remove vault fee on claim rewards for vandals pool

--- programs/cardinal-rewards-center/src/payment/state.rs
+++ programs/cardinal-rewards-center/src/payment/state.rs
@@ -45,13 +45,12 @@ pub fn assert_payment_info(stake_pool: Pubkey, action: Action, payment_info: Pub
         _ => [
             "382KXQfzC26jbFmLZBmKoZ6eRz53iwGfxXwoGyyyH8po".to_string(), // cardinal-test-wsol
             "HqiCY5NqfHfyhyjheQ4ENo5J2XSQBpeqhNoeESkDWBpU".to_string(), // cardinal-test (native)
-            "SdFEeJxn7XxcnYEMNpnoMMSsTfmA1bHfiRdu6qra7zL".to_string(),  // cardinal-default 0.005
+            "SdFEeJxn7XxcnYEMNpnoMMSsTfmA1bHfiRdu6qra7zL".to_string(),  // cardinal-default 0.002
         ]
         .to_vec(),
     };
     let allowed_payment_infos = match (stake_pool.key().to_string().as_str(), action) {
         ("ndu643uUkFBt4YbXgHEfstkU25eEe4kDLjTD5uziEKx", Action::Stake) => ["Ad29pAAdYvYTcDzRHtA1req5an2DqFUfY1s5tkUC88Lr".to_string()].to_vec(), // vandals stake/claim cardinal-vandals
-        ("ndu643uUkFBt4YbXgHEfstkU25eEe4kDLjTD5uziEKx", Action::ClaimRewards) => ["Ad29pAAdYvYTcDzRHtA1req5an2DqFUfY1s5tkUC88Lr".to_string()].to_vec(), // vandals stake/claim cardinal-vandals
         ("ndu643uUkFBt4YbXgHEfstkU25eEe4kDLjTD5uziEKx", Action::Unstake) => ["4LKjUP41DkFy8C4FbZpFBfNtLaSixLEfS5adrFA9Z7CN".to_string()].to_vec(), // vandals unstake cardinal-vandals-2
         _ => default_allowed_payment_infos,
     };
