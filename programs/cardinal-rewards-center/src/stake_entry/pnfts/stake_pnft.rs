57c93cebcada157658811bdf1487ecafe2a8aeb7
Giannis Chatziveroglou <gchatz@mit.edu>
2023-02-03 14:54:47 -0500

    stake pnft

--- programs/cardinal-rewards-center/src/stake_entry/pnfts/stake_pnft.rs
+++ programs/cardinal-rewards-center/src/stake_entry/pnfts/stake_pnft.rs
@@ -1,4 +1,5 @@
 use crate::errors::ErrorCode;
+use crate::escrow_seeds;
 use crate::stake_seed;
 use crate::StakeEntry;
 use crate::StakePool;
@@ -7,10 +8,13 @@ use anchor_lang::prelude::*;
 use anchor_spl::token::Mint;
 use anchor_spl::token::Token;
 use anchor_spl::token::TokenAccount;
+use mpl_token_metadata::instruction::DelegateArgs;
+use mpl_token_metadata::instruction::LockArgs;
 use mpl_token_metadata::instruction::MetadataInstruction;
-use mpl_token_metadata::instruction::TransferArgs;
 use solana_program::instruction::Instruction;
 use solana_program::program::invoke;
+use solana_program::program::invoke_signed;
+use solana_program::sysvar;
 
 #[derive(Accounts)]
 pub struct StakePNFTCtx<'info> {
@@ -23,6 +27,12 @@ pub struct StakePNFTCtx<'info> {
     stake_mint: Box<Account<'info, Mint>>,
     /// CHECK: Checked in handler
     stake_mint_metadata: UncheckedAccount<'info>,
+    /// CHECK: Checked in handler
+    stake_mint_edition: UncheckedAccount<'info>,
+    /// CHECK: Checked in handler
+    stake_token_record_account: UncheckedAccount<'info>,
+    /// CHECK: Checked in handler
+    authorization_rules: UncheckedAccount<'info>,
 
     #[account(mut)]
     user: Signer<'info>,
@@ -40,53 +50,89 @@ pub struct StakePNFTCtx<'info> {
     /// CHECK: Address checked
     #[account(address = cardinal_creator_standard::id())]
     token_metadata_program: UncheckedAccount<'info>,
+    /// CHECK: Address checked
+    #[account(address = &sysvar::instructions::id())]
+    sysvar_instructions: UncheckedAccount<'info>,
+    authorization_rules_program: Program<'info, Token>,
     token_program: Program<'info, Token>,
     system_program: Program<'info, System>,
 }
 
 pub fn handler(ctx: Context<StakePNFTCtx>, amount: u64) -> Result<()> {
+    let user_escrow_seeds = escrow_seeds(&ctx.accounts.user.key(), &ctx.accounts.user_escrow.key())?;
+
     invoke(
         &Instruction {
             program_id: mpl_token_metadata::id(),
             accounts: vec![
                 AccountMeta::new_readonly(mpl_token_metadata::id(), false),
-                AccountMeta::new_readonly(token_manager.key(), false),
-                AccountMeta::new(mint_metadata_info.key(), false),
-                AccountMeta::new_readonly(mint_edition_info.key(), false),
-                AccountMeta::new(recipient_token_record_info.key(), false),
-                AccountMeta::new_readonly(mint_info.key(), false),
-                AccountMeta::new(ctx.accounts.recipient_token_account.key(), false),
-                AccountMeta::new_readonly(ctx.accounts.recipient.key(), true),
-                AccountMeta::new(ctx.accounts.recipient.key(), true),
+                AccountMeta::new_readonly(ctx.accounts.user_escrow.key(), false),
+                AccountMeta::new(ctx.accounts.stake_mint_metadata.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.stake_mint_edition.key(), false),
+                AccountMeta::new(ctx.accounts.stake_token_record_account.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.stake_mint.key(), false),
+                AccountMeta::new(ctx.accounts.user_stake_mint_token_account.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.user.key(), true),
+                AccountMeta::new(ctx.accounts.user.key(), true),
                 AccountMeta::new_readonly(ctx.accounts.system_program.key(), false),
-                AccountMeta::new_readonly(sysvar_instructions_info.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.sysvar_instructions.key(), false),
                 AccountMeta::new_readonly(ctx.accounts.token_program.key(), false),
-                AccountMeta::new_readonly(authorization_rules_program_info.key(), false),
-                AccountMeta::new_readonly(authorization_rules_info.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.authorization_rules_program.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.authorization_rules.key(), false),
             ],
-            data: MetadataInstruction::Delegate(DelegateArgs::LockedTransferV1 {
-                amount: token_manager.amount,
-                locked_address: token_manager.key(),
-                authorization_data: None,
-            })
-            .try_to_vec()
-            .unwrap(),
+            data: MetadataInstruction::Delegate(DelegateArgs::StakingV1 { amount: 1, authorization_data: None }).try_to_vec().unwrap(),
         },
         &[
-            token_manager.to_account_info(),
-            mint_metadata_info.to_account_info(),
-            mint_edition_info.to_account_info(),
-            recipient_token_record_info.to_account_info(),
-            mint_info.to_account_info(),
-            ctx.accounts.recipient_token_account.to_account_info(),
-            ctx.accounts.recipient.to_account_info(),
-            ctx.accounts.recipient.to_account_info(),
+            ctx.accounts.user_escrow.to_account_info(),
+            ctx.accounts.stake_mint_metadata.to_account_info(),
+            ctx.accounts.stake_mint_edition.to_account_info(),
+            ctx.accounts.stake_token_record_account.to_account_info(),
+            ctx.accounts.stake_mint.to_account_info(),
+            ctx.accounts.user_stake_mint_token_account.to_account_info(),
+            ctx.accounts.user.to_account_info(),
             ctx.accounts.system_program.to_account_info(),
-            sysvar_instructions_info.to_account_info(),
+            ctx.accounts.sysvar_instructions.to_account_info(),
             ctx.accounts.token_program.to_account_info(),
-            authorization_rules_program_info.to_account_info(),
-            authorization_rules_info.to_account_info(),
+            ctx.accounts.authorization_rules_program.to_account_info(),
+            ctx.accounts.authorization_rules.to_account_info(),
         ],
+    )?;
+
+    invoke_signed(
+        &Instruction {
+            program_id: mpl_token_metadata::id(),
+            accounts: vec![
+                AccountMeta::new_readonly(ctx.accounts.user_escrow.key(), true),
+                AccountMeta::new_readonly(ctx.accounts.user.key(), false),
+                AccountMeta::new(ctx.accounts.user_stake_mint_token_account.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.stake_mint.key(), false),
+                AccountMeta::new(ctx.accounts.stake_mint_metadata.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.stake_mint_edition.key(), false),
+                AccountMeta::new(ctx.accounts.stake_token_record_account.key(), false),
+                AccountMeta::new(ctx.accounts.user.key(), true),
+                AccountMeta::new_readonly(ctx.accounts.system_program.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.sysvar_instructions.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.token_program.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.authorization_rules_program.key(), false),
+                AccountMeta::new_readonly(ctx.accounts.authorization_rules.key(), false),
+            ],
+            data: MetadataInstruction::Lock(LockArgs::V1 { authorization_data: None }).try_to_vec().unwrap(),
+        },
+        &[
+            ctx.accounts.user_escrow.to_account_info(),
+            ctx.accounts.user.to_account_info(),
+            ctx.accounts.user_stake_mint_token_account.to_account_info(),
+            ctx.accounts.stake_mint.to_account_info(),
+            ctx.accounts.stake_mint_metadata.to_account_info(),
+            ctx.accounts.stake_mint_edition.to_account_info(),
+            ctx.accounts.stake_token_record_account.to_account_info(),
+            ctx.accounts.system_program.to_account_info(),
+            ctx.accounts.sysvar_instructions.to_account_info(),
+            ctx.accounts.token_program.to_account_info(),
+            ctx.accounts.authorization_rules_program.to_account_info(),
+            ctx.accounts.authorization_rules.to_account_info(),
+        ],
+        &[&user_escrow_seeds.iter().map(|s| s.as_slice()).collect::<Vec<&[u8]>>()],
     )?;
 
     Ok(())
