bea0d110c43d9cb25bc35f600bf1ff399ac64287
jpbogle <jpbogle22@gmail.com>
2023-02-11 19:23:43 -0500

    Authorization client code

--- sdk/index.ts
+++ sdk/index.ts
@@ -1,5 +1,6 @@
 export * from "./accounts";
 export * from "./api";
+export * from "./authorization";
 export * from "./constants";
 export * from "./idl/cardinal_rewards_center";
 export * from "./payment";
